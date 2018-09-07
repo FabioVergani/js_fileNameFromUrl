16a2638ee05a71963bc386bfdca7289300edcfce
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-07 17:27:49 +0200

    Create fileNameFromUrl.js

---
+++ fileNameFromUrl.js
@@ -0,0 +1,44 @@
+const fileNameFromUrl=x=>{
+	let s=x,l=s.length,a=false,b=false;
+	if(l>1){
+		let i=s.lastIndexOf('/');
+		if(i!==-1 && ++i!==l){
+			s=s.substring(i,l);
+			if(l=s.length!==0){
+				i=s.lastIndexOf('.');
+				if(i!==-1 && i!==--l){
+					a=s.substring(0,i);
+					s=s.substring(++i);
+					if(l=s.length!==0){
+						i=s.lastIndexOf('?');
+						if(i!==-1 && i!==--l){
+							estensione=s.substring(0,i)
+						}
+					}
+				}
+			}
+		}
+	};
+	return [a,b]//nome,estensione
+};
+
+
+/*
+
+//test:
+[
+'.',
+'a.',
+'',
+'/',
+'https://www.google.it/images/branding/googlelogo/2x/',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.pngz',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.pngz?',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.a?',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.?',
+'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.pngz?v3'
+].forEach(x=>{console.log(x,fileNameFromUrl(x).join('\n'))});
+
+*/
