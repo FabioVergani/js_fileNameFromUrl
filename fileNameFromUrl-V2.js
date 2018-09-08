418fbf80f877e64a691a6f9278ed26c3645c62e9
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-09-08 14:43:40 +0200

    Update fileNameFromUrl-V2.js

--- fileNameFromUrl-V2.js
+++ fileNameFromUrl-V2.js
@@ -1,32 +1,30 @@
 const fileNameFromUrl=t=>{
 	let l,i,s=t,a=false,b=false;
-	const f=x=>{
-	return (l=s.length)>1 && (i=s.lastIndexOf(x))!==-1 && --l!==i
-	};
+	const f=x=>(l=s.length)>1&&(i=s.lastIndexOf(x))!==-1&&--l!==i;
 	if(f('/')){
-		s=s.substring(i);
+		s=s.substring(++i);
 		if(f('.')){
 			a=s.substring(0,i);
-			s=s.substring(++i);
-			if(i!==l){
+			if(++i!==l){
+				s=s.substring(i);
 				b=s.substring(0,f('?')?i:l)
 			}
 		}
 	};
 	return [a,b]//nome,estensione
 };
-//
+//Test:
 [
 '.',
 'a.',
 '',
 '/',
-'https://www.google.it/images/branding/googlelogo/2x/',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.pngz',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.pngz?',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.a?',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.?',
-'https://www.google.it/images/branding/googlelogo/2x/googlelogo_color_272x92dp.pngz?v3'
-].forEach(x=>{console.log(x,'\n',fileNameFromUrl(x).join('\n'))});
+'https://www.pippo.it/images/dir/dir/',
+'https://www.pippo.it/images/dir/dir/logo',
+'https://www.pippo.it/images/dir/dir/logo.',
+'https://www.pippo.it/images/dir/dir/logo.pngz',
+'https://www.pippo.it/images/dir/dir/logo.pngz?',
+'https://www.pippo.it/images/dir/dir/logo.a?',
+'https://www.pippo.it/images/dir/dir/logo.?',
+'https://www.pippo.it/images/dir/dir/logo.pngz?v3'
+].forEach(t=>{console.log(t,'\n',fileNameFromUrl(t).join('\n'))});
